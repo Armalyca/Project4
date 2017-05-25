008b2ba7e59e1d048b0e0c4811fb69bd2b145085
Armalyca <mat.kerdiles@free.fr>
2017-05-26 00:10:31 +0200

    Update functions.h

---
+++ functions.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void diplay_current_date(Date* Cdate)
+void change_current_Date(Date* Cdate)
+void read_menu_librarian()
+void read_menu_librarian2()
+void read_menu_librarian3()
+void interact_menu_librarian()
