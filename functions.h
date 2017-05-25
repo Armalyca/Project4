d1eb48fb2ec967ed3859b1236536f5f35e01462d
Armalyca <mat.kerdiles@free.fr>
2017-05-26 01:08:12 +0200

    Update functions.h
    normalement y a toutes les fonctions

--- functions.h
+++ functions.h
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void diplay_current_date(Date* Cdate)
-void change_current_Date(Date* Cdate)
-void read_menu_librarian()
-void read_menu_librarian2()
-void read_menu_librarian3()
-void interact_menu_librarian()
+
+void AddMember();
+int SuppressMember();
+void AddBook();
+int SuppressBook();
+int NewLoan();
+void LateBooks();
+void disp_books_ByCode();
+void disp_books_ByTitle();
+void disp_books_ByAuthor();
+void search_book_ByCode();
+void search_book_ByTitle();
+void search_book_ByAuthor();
+void change_current_Date(struct Date* Cdate);
+void diplay_current_date(struct Date* Cdate);
